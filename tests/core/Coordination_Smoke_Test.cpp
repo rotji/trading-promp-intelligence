796fed590348097b87e127dd2585cbf422b79a8e
rotji john <starrotji@gmail.com>
2026-08-19 02:55:06 +0100

    Add coordination aggregation smoke test

---
+++ tests/core/Coordination_Smoke_Test.cpp
@@ -0,0 +1,9 @@
+#include "core/Core.h"
+#include "core/Coordination.h"
+
+static_assert(sizeof(gcfios::core::Coordination::EventContract) > 0);
+static_assert(sizeof(gcfios::core::Coordination::ExecutionDispatchContract) > 0);
+
+int main() {
+    return 0;
+}
