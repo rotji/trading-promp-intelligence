92ec79acbfd1af56c77e005d8136d88d6158bee1
rotji john <starrotji@gmail.com>
2026-08-19 02:34:51 +0100

    Expose execution routing through Core.h

--- src/core/Core.h
+++ src/core/Core.h
@@ -10,6 +10,7 @@
 #include "core/execution/ExecutionAdmission.h"
 #include "core/execution/ExecutionDispatch.h"
 #include "core/execution/ExecutionLifecycle.h"
+#include "core/execution/ExecutionRouting.h"
 #include "core/foundation/Foundation_Error.h"
 #include "core/foundation/Foundation_Result.h"
 #include "core/foundation/Foundation_Status.h"
